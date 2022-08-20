4e9501d8b3ba743bd5cd4356c2318d9ca170fc8b
Mahmut ARABACI <102673240+marabac1@users.noreply.github.com>
2022-08-20 20:10:43 +0300

    used switch instead of if

--- homework4.go
+++ homework4.go
@@ -13,38 +13,41 @@ func main() {
 	fmt.Println("Meyveleri rasgele baştan sona yada sondan başa yazan programa hoşgeldiniz")
 	insertFruitIntoArray(&rndName)
 	for {
-		rng := rand.Intn(1)
-		rbg := false
-		if rng == 1 {
-			rbg = true
-		} else {
-			rbg = false
-		}
-		arrPrinter(rndName, rbg)
+		arrPrinter(rndName, rand.Intn(2))
 		fmt.Println("Devam etmek istiyor musunuz ? e/h")
 		chc := "h"
-		fmt.Scanf(chc)
-		if chc == "e" || chc == "h" {
-			if chc == "e" {
-				fmt.Println("Program Devam Ediyor...")
-			} else {
-				fmt.Println("Program Sonlandırılıyor....")
-				break
-			}
-		} else {
-			fmt.Println("Yanlış Giriş Yaptınız Başa Dönülüyor...")
+		fmt.Scanln(&chc)
+		if loopcheck(chc) {
+			break
 		}
-
 	}
 
 }
 
-func arrPrinter(arr []string, reverse bool) {
+func loopcheck(chc string) bool {
+	stp := false
+	switch chc {
+	case "e":
+		fmt.Println("Program Devam Ediyor...")
+		break
+	case "h":
+		fmt.Println("Program Sonlandırılıyor....")
+		stp = true
+		break
+	default:
+		fmt.Println("Yanlış Giriş Yaptınız Başa Dönülüyor...")
+		break
+	}
+	return stp
+
+}
+
+func arrPrinter(arr []string, reverse int) {
 	if len(arr) == 0 {
 		fmt.Println("Dizi Boş")
 	} else {
-		if reverse {
-			for i := len(arr) - 1; i == 0; i-- {
+		if reverse == 1 {
+			for i := len(arr) - 1; i >= 0; i-- {
 				fmt.Println(arr[i])
 			}
 		} else {
