77429283f2b42c5bfd352ac2a8db095005310c64
Mahmut ARABACI <102673240+marabac1@users.noreply.github.com>
2022-08-23 18:55:34 +0300

    Create sign.go

---
+++ sign.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	printMySign()
+}
+
+func printMySign() {
+	fmt.Println("                                                                                                   ")
+	fmt.Println("	>=>       >=>       >>       >======>           >>       >=>>=>          >>           >=>     >=>	")
+	fmt.Println("	>> >=>   >>=>      >>=>      >=>    >=>        >>=>      >>   >=>       >>=>       >=>   >=>  >=>	")
+	fmt.Println("	>=> >=> > >=>     >> >=>     >=>    >=>       >> >=>     >>    >=>     >> >=>     >=>         >=>	")
+	fmt.Println("	>=>  >=>  >=>    >=>  >=>    >> >==>         >=>  >=>    >==>>=>      >=>  >=>    >=>         >=>	")
+	fmt.Println("	>=>   >>  >=>   >=====>>=>   >=>  >=>       >=====>>=>   >>    >=>   >=====>>=>   >=>         >=>	")
+	fmt.Println("	>=>       >=>  >=>      >=>  >=>    >=>    >=>      >=>  >>     >>  >=>      >=>   >=>   >=>  >=>	")
+	fmt.Println("	>=>       >=> >=>        >=> >=>      >=> >=>        >=> >===>>=>  >=>        >=>    >===>    >=>	")
+	fmt.Println("                                                                                                   ")
+} // TODO Maybe make it public and import in other projects for quick signature
